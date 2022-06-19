b12bcce83db7a2a81d253ef655056fa6b0e02704
MinxunYuan <515734926@qq.com>
2022-06-19 19:55:48 +0800

    finish generateInvertedIndex()

--- invertedIndex.c
+++ invertedIndex.c
@@ -5,7 +5,8 @@
 #include "invertedIndex.h"
 
 #define MAX_WD_SIZE 100
-#define COLLECTION_FILE_NAME "collection1.txt"
+#define MAX_LINE_SIZE 512
+#define COLLECTION_FILE_NAME "collection.txt"
 
 #define isInValidPunc(c) ((c) == '.' || (c) == ',' || (c) == ';' || (c) == '?')
 /// Helper function prototypes
@@ -25,96 +26,77 @@ typedef struct wf {
     // struct wf* next;
 } wf;
 
-// wf* wf_create(char* word, wf* next) {
-//     wf* node = malloc(sizeof(wf));
-//     (*node) = (wf){
-//         .word = strdup(word),
-//         .freq = 1,
-//         .next = next,
-//     };
-//     return node;
-// }
-
-// void wf_add(wf* head, char* word) {
-//     wf* cur = head;
-//     while (cur) {
-//         if (strcmp(cur->word, word)) {
-//             cur->freq++;
-//             return;
-//         }
-//         else
-//             cur = cur->next;
-//     }
-//     head
-
-// }
 
 int main() {
     // 为啥char *s = "  123  "这样就不行啊
-    char s[MAX_WD_SIZE];
+    // char s[MAX_WD_SIZE];
 
     // sprintf(s, "%s", "abc.net.au.");
 
     // s = normaliseWord(s);
     // printf("%s\n", normaliseWord(s));
-    // InvertedIndexBST inverted_index = generateInvertedIndex(COLLECTION_FILE_NAME);
 
-    printf("testing generateInvertedIndex\n");
+    InvertedIndexBST inverted_index = generateInvertedIndex(COLLECTION_FILE_NAME);
+    // printf("\n");
+    // printf("%s %s\n", inverted_index->right->word, inverted_index->right->fileList->filename);
+
+    return 0;
+}
+
+/**
+ * This function opens the collection file with the given name, and then
+ * generates an inverted index from those files listed in the collection
+ * file,  as  discussed  in  the spec. It returns the generated inverted
+ * index.
+ */
+InvertedIndexBST generateInvertedIndex(char* collectionFilename) {
+    InvertedIndexBST bst = NULL;
 
     // reads data from a given collection of files in collection.txt
-    char fileNanes[MAX_WD_SIZE];
-    FILE* fp = fopen(COLLECTION_FILE_NAME, "r");
+    char file_names[MAX_WD_SIZE];
+    FILE* fp = fopen(collectionFilename, "r");
     int c, i = 0;
+    // 把空格换行什么的去掉，读到一个buffer里面
     while ((c = fgetc(fp)) != EOF) {
-        if (!isspace(c)) {
-            fileNanes[i++] = c;
-        }
+        if (!isspace(c)) file_names[i++] = c;
     }
+    // finish reading collectionFilename
     fclose(fp);
 
-    // 搓一个tok可以是str的strtok
-    char* pos;
-    char fileName[MAX_WD_SIZE];
+    // 搓一个tok可以是str的strtok()
+    // char* pos;
+    char* pos = strstr(file_names, ".txt");
+    char file_name[MAX_WD_SIZE];
 
     // while (ch < fileNanes + strlen(fileNanes)) {
-    for (char* ch = fileNanes; ch < fileNanes + strlen(fileNanes);) {
-        memset(fileName, 0, sizeof(fileName));
-        pos = strstr(ch, ".txt");
-        if (!pos) break;
+    for (char* ch = file_names; pos && ch < file_names + strlen(file_names); ch = pos + 4, pos = strstr(ch, ".txt")) {
+        memset(file_name, 0, sizeof(file_name));
 
+        // len(each_file) without suffix
         int len = pos - ch;
+        for (int i = 0; i < len; i++) file_name[i] = ch[i];
 
-        // pos-ch为去掉.txt的那一段fileName（我寻思不可能有.txt.txt这种东西吧）
-        for (int i = 0; i < len; i++) fileName[i] = ch[i];
-        // printf("%d", strlen(fileName));
+        strcat(file_name, ".txt"); // file1 -> file1.txt
+        // read each file in collectionFilename
+        FILE* each_file = fopen(file_name, "r");
 
-        // 到这里相当于就已经有fileName了，然后直接strcat
-        strcat(fileName, ".txt");
-        // 拿到FileName之后进去读
-        printf("%s\n", fileName);
+        char line[MAX_LINE_SIZE];
 
-        FILE* each_file = fopen(fileName, "r");
-        // no need to impose any restriction on a line length
-        char line[512];
-        // each_file里单词总数wd_cnt去重后的wf_cnt
-        int wd_cnt = 0, wf_cnt = 0;
-        // set up variables for iterating over tokens ' ' '\n'
+        int wd_cnt = 0, wf_cnt = 0; // each_file里单词总数wd_cnt去重后的wf_cnt用来统计词频
+        // set up variables for iterating over tokens ' ' and '\n'
         char *wd, *input;
 
-        // 算了，each_file直接配一个wf数组统计词频算tf好了
-        wf wfs[100];
+        wf wfs[100]; // TODO:
         memset(wfs, 0, sizeof(wfs));
 
-        // each_file的每一行
-        while (fgets(line, 512, each_file) != NULL) {
+        while (fgets(line, MAX_LINE_SIZE, each_file) != NULL) {
             input = strdup(line); // backup
             while ((wd = strsep(&input, " \n")) != NULL) {
                 // eliminate blank space and line
                 if ((isalnum(*wd))) {
                     wd_cnt++;
-
+                    // normalise word before add
                     wd = normaliseWord(wd);
-                    // printf("%s\n", wd);
                     // 先去wfs里遍历一下康康有没有之前见过的
                     int wf_num;
                     for (wf_num = 0; wf_num < wf_cnt; wf_num++) {
@@ -124,47 +106,27 @@ int main() {
                         }
                     }
 
-                    // 没有
+                    // no
                     if (wf_num == wf_cnt) {
                         wf_cnt++;
                         wfs[wf_num].word = strdup(wd);
                         wfs[wf_num].freq++;
                     }
-
-                    // Before inserting words in your inverted index, you need to normaliseWord
-                    // wd = normaliseWord(wd);
-                    // add word to fileListNode
                 }
             }
         }
-        printf("%d %d\n", wd_cnt, wf_cnt);
+
+        // printf("wd_cnt %d wf_cnt %d\n", wd_cnt, wf_cnt);
         for (int i = 0; i < wf_cnt; i++) {
             // create InvertedIndexNode
-            // 到这里
-            // bst = invertedIndexInsert(bst, wfs[i].word, fileName, (double)wfs[i].freq / wd_cnt);
-            printf("%s %d, tf=%f\n", wfs[i].word, wfs[i].freq, (double)wfs[i].freq / wd_cnt);
+            bst = invertedIndexInsert(bst, wfs[i].word, file_name, (double)wfs[i].freq / wd_cnt);
+            // printf("%s %d, tf=%f\n", wfs[i].word, wfs[i].freq, (double)wfs[i].freq / wd_cnt);
         }
-
         // grab next file
         wf_cnt = wd_cnt = 0;
         fclose(each_file);
-        // find next file name
-        ch = pos + 4;
     }
-
-    InvertedIndexBST bst = NULL;
-
-    puts("\ntesting bst...");
-    // same word
-    bst = invertedIndexInsert(bst, "ha", "file1.txt", 0.01);
-    bst = invertedIndexInsert(bst, "hahaha", "file2.txt", 0.01);
-    bst = invertedIndexInsert(bst, "hahaha", "file1.txt", 0.01);
-
-    // same word, different file
-    printf("%s %s\n", bst->word, bst->fileList->filename);
-    printf("%s %s\n", bst->right->word, bst->right->fileList->filename);
-
-    return 0;
+    return bst;
 }
 
 /**
@@ -190,16 +152,6 @@ char* normaliseWord(char* str) {
 
     return i;
 }
-
-/**
- * This function opens the collection file with the given name, and then
- * generates an inverted index from those files listed in the collection
- * file,  as  discussed  in  the spec. It returns the generated inverted
- * index.
- */
-// InvertedIndexBST generateInvertedIndex(char *collectionFilename) {
-
-// }
 
 // InvertedIndexBST findInvertedIndexNodeByWord(InvertedIndexBST root, char* word) {
 //     if (!root) return NULL;
@@ -230,7 +182,7 @@ InvertedIndexBST invertedIndexInsert(InvertedIndexBST root, char* word, char* fi
         // printf("get1");
         return createInvertedIndexNode(word, fileName, tf);
     }
-    printf("find word\n");
+    // printf("find word\n");
     if (strcmp(word, root->word) < 0)
         root->left = invertedIndexInsert(root->left, word, fileName, tf);
     else if (strcmp(word, root->word) > 0)
@@ -246,7 +198,7 @@ InvertedIndexBST invertedIndexInsert(InvertedIndexBST root, char* word, char* fi
  */
 // struct InvertedIndexNode* createInvertedIndexNode(char* wd, char* fileName) {
 struct InvertedIndexNode* createInvertedIndexNode(char* wd, char* fileName, double tf) {
-    printf("createInvertedIndexNode\n");
+    // printf("createInvertedIndexNode\n");
     struct InvertedIndexNode* node = (struct InvertedIndexNode*)malloc(sizeof(struct InvertedIndexNode));
 
     // node->word = strdup(wd);
@@ -279,10 +231,10 @@ struct FileListNode* createFileListNode(char* fileName, double tf) {
 FileList insertFileNode(FileList flst, char* fileName, double tf) {
     // 遍历each_file已经对word去重，所以放心insert
     // if (findFileListNode(flst, fileName)) return;
-    printf("insertFileNode,");
+    // printf("insertFileNode,");
 
     if (!flst) return createFileListNode(fileName, tf);
-    printf("create a FileNode\n");
+    // printf("create a FileNode\n");
     FileList head = flst, prev;
 
     // 如果fileName大于flst->filename，就往后走
