4e648db540772f9321b2aa3f98954f3643a1bb1a
jerry <515734926@qq.com>
2022-06-22 16:20:04 +0800

    init commit

---
+++ invertedIndex.c
@@ -0,0 +1,40 @@
+// COMP2521 Assignment 1
+
+#include <assert.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "invertedIndex.h"
+
+// Part 1
+
+InvertedIndexBST generateInvertedIndex(char *collectionFilename) {
+	return NULL;
+}
+
+void printInvertedIndex(InvertedIndexBST tree, char *filename) {
+
+}
+
+void freeInvertedIndex(InvertedIndexBST tree) {
+
+}
+
+// Part 2
+
+TfIdfList searchOne(InvertedIndexBST tree, char *searchWord, int D) {
+	return NULL;
+}
+
+TfIdfList searchMany(InvertedIndexBST tree, char *searchWords[], int D) {
+	return NULL;
+}
+
+void freeTfIdfList(TfIdfList list) {
+
+}
+
